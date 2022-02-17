edb29492ab2b78743806ac598b0d6024d9fe6088
stevengm45 <stevengm45@gmail.com>
2022-02-17 12:08:10 -0500

    validation is added in challenge 1

--- reto_1/script.js
+++ reto_1/script.js
@@ -2,26 +2,29 @@
 
 // funcion calcular cuenta total
 
-function calcularTotalCuenta(){
+function calcularTotalCuenta() {
     var valorCuenta = document.getElementById('box-cuenta').value;
 
     var porcentajePropina = document.getElementById('box-propina').value;
 
+    valorCuenta = parseInt(valorCuenta)
+    porcentajePropina = parseInt(porcentajePropina)
+
     var propina = valorCuenta * (porcentajePropina / 100);
 
-    var totalCuenta = parseInt(propina) + parseInt(valorCuenta)
+    var totalCuenta = parseInt(propina) + valorCuenta
 
-    if (valorCuenta == "" || porcentajePropina == "") {
-        document.getElementById('valor-cuenta-total').innerHTML = "Ingrese todos los valores"
-    } else {
+    if (!Number.isInteger(valorCuenta) || !Number.isInteger(porcentajePropina)) {
+        alert("Ingrese números")
+    }
+    else {
         document.getElementById('valor-cuenta').innerHTML = parseInt(valorCuenta).toFixed(1);
         document.getElementById('propina-total').innerHTML = parseInt(propina).toFixed(1);
         document.getElementById('valor-cuenta-total').innerHTML = totalCuenta.toFixed(1);
     }
-
 }
 
 // funcion limpiar boton
 function limpiarFormulario() {
     document.getElementById("my-form").reset();
-  }
+}
